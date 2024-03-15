709525769ce27b4b84da058bee850695599c4a10
Kelvin Kandie <134844713+Kandie19@users.noreply.github.com>
2024-03-15 17:21:36 +0300

    Create HomePage.js

---
+++ src/HomePage.js
@@ -0,0 +1,14 @@
+// src/HomePage.js
+
+import React from 'react';
+
+function HomePage() {
+  return (
+    <div>
+      <h1>Welcome to Taskify!</h1>
+      <p>Do It Like A Boss.</p>
+    </div>
+  );
+}
+
+export default HomePage;
